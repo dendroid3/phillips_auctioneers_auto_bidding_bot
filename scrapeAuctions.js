78451536c726117369672662bf30fccbd6e2cb17
Wanjohi <kazibin66@gmail.com>
2025-06-23 12:23:21 +0300

    remove executionable path to let script use puppeteer's inate browser

--- scrapeAuctions.js
+++ scrapeAuctions.js
@@ -45,7 +45,7 @@ const scrape = async () => {
     console.log("Starting scrape...");
     const browser = await puppeteer.launch({
         // executablePath: '/snap/bin/chromium', 
-        executablePath: '/usr/bin/google-chrome',
+        // executablePath: '/usr/bin/google-chrome',
         args: ['--no-sandbox', '--disable-setuid-sandbox'], 
         headless: true // Use the new headless mode
     });
